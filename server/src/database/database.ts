f889e342adb322408b208fbe1c41b7707f3b827d
JoaoLucas23 <jllgoncalves23@gmail.com>
2022-11-18 15:17:45 -0300

    a

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const user = process.env.DB_USER || "";
+const password = process.env.DB_PASSWORD || "";
 
 export const sequelize = new Sequelize(
     'app_copa',
